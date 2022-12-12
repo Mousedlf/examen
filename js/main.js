60f56ef91829ea43e4f10a87cc6f8a643d375056
Mousedlf <dlfcaroline@gmail.com>
2022-12-12 16:42:01 +0100

    send, edit, delete works comments don't (only shown in console)

--- js/main.js
+++ js/main.js
@@ -12,25 +12,23 @@ const registerPopUp = document.querySelector('.register')
 const sendButton = document.querySelector("#sendPost")
 sendButton.addEventListener("click", createPost)
 
-
-
 const mainContainer = document.querySelector('#mainContainer')
 
 let token = null
 let myUsername;
 
+
+// VIDER ET AFFICHER
 function clearMainContainer(){
-    mainContainer.innerHTML=""
+    mainContainer.innerHTML= ""
 }
-
-
-const refreshMessages = document.querySelector('#refresh')
-refreshMessages.addEventListener('click', displayPosts)
-
 function display(content){
     clearMainContainer()
     mainContainer.innerHTML=content
 }
+const refreshMessages = document.querySelector('#refresh')
+refreshMessages.addEventListener('click', displayPosts)
+
 
 // TEMPLATES
 function getPostTemplate(post){
@@ -46,7 +44,7 @@ function getPostTemplate(post){
     let template
         if(post.user.username == myUsername){
             template = `
-                    <div class="card m-3">
+                    <div id="${post.id}" class="card m-3">
                           <div class="card-header d-flex justify-content-between bg-dark text-white align-items-center">
                                <h6 class="card-title mb-0">${post.user.username}</h6>
                                <div>
@@ -56,26 +54,24 @@ function getPostTemplate(post){
                           </div>
                           <div class="card-body d-flex flex-column">
                               <p class="mb-2">${post.content}</p>
-                              <a href="#" class="btn btn-link mb-4 text-end">See more</a>
-                              <div>
-                                  <div class="input-group mb-3">
+                              <a href="#" class="seeMore btn btn-link mb-4 text-end">See more</a>
+                              <div class="input-group mb-3">
                                   <input type="text" class="postContent form-control" placeholder="Type in your new and improved message then press on the pen">
-                                   <span class="input-group-text" id="basic-addon2"><i id="${post.id}" class="edit fa-solid fa-pen"></i></span>
-                                   </div>
+                                  <span class="input-group-text" id="basic-addon2"><i id="${post.id}" class="edit fa-solid fa-pen"></i></span>
                               </div>
                           </div>
                     </div>
                     `
         } else {
             template = `
-                    <div class="card m-2">
+                    <div id="${post.id}" class="card m-2">
                           <div class="card-header d-flex justify-content-between bg-success align-items-center">
                                <h6 class="card-title mb-0">${post.user.username}</h6>
                                <p class="badge fw-normal mb-0">${time}, ${date}</p>
                           </div>
                           <div class="card-body d-flex flex-column">
                               <p class="mb-2">${post.content}</p>
-                              <a href="#" class="btn btn-link text-secondary text-end">See more</a>
+                              <a id="${post.id}" href="#" class="seeMore btn btn-link text-secondary text-end">See more</a>
                           </div>
 
                     </div>
@@ -91,6 +87,39 @@ function getPostsTemplate(posts) {
         })
         return postsTemplate
 }
+function getCommentTemplate(comment){
+    let template
+    if(comment.user.username == myUsername){
+        template = `
+                    <div class="card m-3">
+                <div class="card-body d-flex flex-column">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <h6 class="card-title mb-0 fw-bold">${comment.user.username}:</h6>
+                            <p class="mb-2">${comment.content}</p>
+                        </div>
+                        <i id="${comment.id}" class="delete ms-2 fa-solid fa-trash"></i>
+                    </div>
+                    <div class="input-group mt-3">
+                        <input type="text" class="postContent form-control" placeholder="Type in your new comment then press on the pen">
+                        <span class="input-group-text" id="basic-addon2"><i id="${comment.id}" class="edit fa-solid fa-pen"></i></span>
+                    </div>
+                </div>
+            </div>
+                    `
+    } else {
+        template = `
+                    <div class="card m-3">
+                        <div class="card-body d-flex flex-column">
+                            <h6 class="card-title mb-0 fw-bold">${comment.user.username}:</h6>
+                            <p class="mb-2">${comment.content}</p>
+                        </div>
+                    </div>
+                    `
+    }
+    return template
+}
+
 
 // FETCH POSTS
 async function getPostsFromAPI(){
@@ -108,15 +137,6 @@ async function getPostsFromAPI(){
             return posts
         })
 }
-
-
-// FAIRE APPARAITRE REGISTER
-const displayRegisterPageButton = document.querySelector("#displayRegisterPage")
-displayRegisterPageButton.addEventListener("click", displayRegistry)
-function displayRegistry() {
-    registerPopUp.classList.remove('d-none')
-}
-
 
 // AFFICHER LES POSTS
 async function displayPosts() {
@@ -140,7 +160,65 @@ async function displayPosts() {
                 editMyPost(editButton.id)
             })
         })
+
+        // All "see more buttons"
+        const seeMoreButtons = document.querySelectorAll('.seeMore')
+        seeMoreButtons.forEach(seeMoreButton =>{
+            seeMoreButton.addEventListener('click', ()=>{
+               console.log("vers page unique pour article")
+                displayOnePost(seeMoreButton.id)
+            })
+        })
     })
+}
+
+// AFFICHER UN SEUL POST
+function displayOnePost(id){
+    let url = `${baseURL}b1devweb/api/posts/${id}`
+    let fetchParams = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+        }
+    }
+    fetch(url, fetchParams)
+        .then(response => response.json())
+        .then(post => {
+            let comments = post.comments
+            comments.forEach(comment =>{
+                console.log(comment)
+                getCommentTemplate(comment)
+            })
+            display(getPostTemplate(post))
+
+        })
+}
+
+
+let messagesAndMessageField = ""
+
+getMessagesFromApi().then(messages=>{
+
+
+    messagesAndMessageField+=getMessagesTemplate(messages)
+    messagesAndMessageField+=getMessageFieldTemplate()
+
+    display(messagesAndMessageField)
+
+    const messageField = document.querySelector("#messageField")
+
+    const sendButton = document.querySelector("#sendMessage")
+    sendButton.addEventListener("click", sendMessage)
+
+
+})
+
+
+// FAIRE APPARAITRE REGISTER
+const displayRegisterPageButton = document.querySelector("#displayRegisterPage")
+displayRegisterPageButton.addEventListener("click", displayRegistry)
+function displayRegistry() {
+    registerPopUp.classList.remove('d-none')
 }
 
 // INSCRIPTION
@@ -198,6 +276,7 @@ function loginToGetAToken() {
 }
 
 
+
 // CREER POST
 function createPost(){
     let url = `${baseURL}b1devweb/api/post`
@@ -214,11 +293,12 @@ function createPost(){
         body: bodySerialise
     }
     fetch(url, fetchParams)
-        displayPosts()
-        messageField.value = ""
+    displayPosts()
+    console.log('choucroute')
+    messageField.value = ""
 }
 
-// EFFACER POST : fonctionne mais faut reload la page à chaque fois..
+// EFFACER POST
 function deleteMyPost(id){
     let url = `${baseURL}b1devweb/api/posts/${id}`
     let fetchParams = {
@@ -256,4 +336,68 @@ function editMyPost(id){
         })
 }
 
+
+/*
+// COMMENTER UN POST
+function commentPost(postId){
+    let url = `${baseURL}b1devweb/api/comment/${postIid}`
+    let body = {
+        content: messageField.value
+    }
+    let bodySerialise = JSON.stringify(body)
+    let fetchParams = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body: bodySerialise
+    }
+    fetch(url, fetchParams)
+    console.log('wut am i doing')
+    getCommentTemplate()
+}
+
+// DELETE COMMENT
+function deleteMyComment(id){
+    let url = `${baseURL}b1devweb/api/comments/${id}`
+    let fetchParams = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    }
+    return  fetch(url, fetchParams)
+        .then(response => response.json())
+        .then(data => {
+            console.log('delete comment')
+        })
+}
+
+// EDIT COMMENT
+function editMyComment(id){
+    const commentContent = document.querySelector('.commentContent')
+    let url =`${baseURL}b1devweb/api/posts/${id}`
+    let body = {
+        content: commentContent.value
+    }
+    let fetchParams = {
+        method : 'PUT',
+        headers:{
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        body : JSON.stringify(body)
+    }
+    fetch(url, fetchParams)
+        .then(response => response.json())
+        .then(data =>{
+            console.log(data)
+            console.log('edit comment')
+            //displayPosts()
+        })
+}
+
+*/
 
